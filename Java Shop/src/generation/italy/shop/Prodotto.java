6ea4202b055805dcabdb252a9a1e0c845486d4bb
Marco Cafasso <marcocafassog@gmail.com>
2022-11-18 15:14:24 +0100

    feat: add methods to return price and price with tax

--- Java Shop/src/generation/italy/shop/Prodotto.java
+++ Java Shop/src/generation/italy/shop/Prodotto.java
@@ -18,5 +18,14 @@ public class Prodotto {
 		Random rnd = new Random();
 		this.codice = rnd.nextInt(00000,99999);
 	}
+	
+	public int mostraPrezzo() {
+		return prezzo;
+	}
+	
+	public int prezzoConIva() {
+		int prezzoIva = prezzo + ((prezzo * iva) / 100);
+		return prezzoIva;
+	}
 
 }
