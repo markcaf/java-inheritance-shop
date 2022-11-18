6abc07d993a9f4442d66dde3c98a182463f721f8
Marco Cafasso <marcocafassog@gmail.com>
2022-11-18 15:12:06 +0100

    feat: add constructor

--- Java Shop/src/generation/italy/shop/Prodotto.java
+++ Java Shop/src/generation/italy/shop/Prodotto.java
@@ -1,5 +1,7 @@
 package generation.italy.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 	
 	private int codice;
@@ -7,5 +9,14 @@ public class Prodotto {
 	private String marca;
 	private int prezzo;
 	private int iva;
+	
+	public Prodotto(String nome, String marca, int prezzo) {
+		this.nome = nome;
+		this.marca = marca;
+		this.prezzo = prezzo;
+		this.iva = 20;
+		Random rnd = new Random();
+		this.codice = rnd.nextInt(00000,99999);
+	}
 
 }
