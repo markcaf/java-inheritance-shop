04596275cc0d3080c0e3e3b5fc5f74e1740971f6
Marco Cafasso <marcocafassog@gmail.com>
2022-11-18 15:18:41 +0100

    feat: add getters and setters

--- Java Shop/src/generation/italy/shop/Prodotto.java
+++ Java Shop/src/generation/italy/shop/Prodotto.java
@@ -28,4 +28,42 @@ public class Prodotto {
 		return prezzoIva;
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(int prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public int getIva() {
+		return iva;
+	}
+
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+
+	public int getCodice() {
+		return codice;
+	}
+	
+	
+
 }
