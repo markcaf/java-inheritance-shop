11598030f495309cf335b24a96bb8ceb96ef544c
Marco Cafasso <marcocafassog@gmail.com>
2022-11-18 16:00:27 +0100

    test: print of a new Smartphone object
    using the override method toString in subclass Smartphone (extends Prodotto)

--- Java Shop/src/generation/italy/shop/Main.java
+++ Java Shop/src/generation/italy/shop/Main.java
@@ -5,18 +5,25 @@ public class Main {
 	public static void main(String[] args) {
 
 		Prodotto prodotto1 = new Prodotto(
-				"FENDER Classic Vibe '70s Telecaster Deluxe",
-				"Fender",
+				"POCO X4 NFC",
+				"Xiaomi",
 				420);
 		
 		Prodotto prodotto2 = new Prodotto(
-				"FENDER FSR Affinity Stratocaster",
-				"Fender",
-				299);
+				"TV 4K Res 2501",
+				"LG",
+				1299);
+		
+		Smartphone tel1 = new Smartphone(
+				"S12 Ultra", 
+				"Samsung", 
+				660, 
+				1234512340, 
+				1024);
 		
 		System.out.println(prodotto1.toString());
-		System.out.println();
 		System.out.println(prodotto2.toString());
+		System.out.println(tel1.toString());
 
 	}
 
