86babf01fa78e1ca141ac8225d9447256d15bda0
Marco Cafasso <marcocafassog@gmail.com>
2022-11-18 15:59:14 +0100

    feat: create the subclass Smartphone.java
    extends the class Prodotto with two additional variables

---
+++ Java Shop/src/generation/italy/shop/Smartphone.java
@@ -0,0 +1,27 @@
+package generation.italy.shop;
+
+public class Smartphone extends Prodotto {
+	
+	private int codiceImei;
+	private int memoria;
+
+	public Smartphone(String nome, String marca, int prezzo, int codiceImei, int memoria) {
+		super(nome, marca, prezzo);
+		this.codiceImei = codiceImei;
+		this.memoria = memoria;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Tipo prodotto: Smartphone"
+		+ "\nCodice: " + getCodice()
+		+ "\nNome: " + getNome()
+		+ "\nMarca: " + getMarca()
+		+ "\nCodice IMEI: " + codiceImei
+		+ "\nMemoria: " + memoria +" GB"
+		+ "\nPrezzo iva esclusa: " + mostraPrezzo()
+		+ "\nPrezzo iva inclusa: " + prezzoConIva() + "\n";
+	}
+
+}
